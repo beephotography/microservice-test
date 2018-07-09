5eb80894a85da4e01496c554a818f96411bb0bf4
Andy Bee <andy@bee-photography.de>
2018-07-09 20:41:22 +0200

    update

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,12 @@
 FROM php:7.2-apache
+
+RUN apt-get update && apt-get install -y \
+    mysql-client \
+    && docker-php-ext-install \
+    pdo_mysql \
+    && rm -rf /var/lib/apt/lists/*
+
+
+
 COPY ./www /var/www/html
 WORKDIR /var/www/html
--- www/index.php
+++ www/index.php
@@ -1,3 +1,42 @@
 <?php
+print '<pre>';
+echo  "hello, world" . PHP_EOL;
 
-echo  "hello, world";
+$host = $_ENV["HOST"] ?? 'localhost';
+$port = $_ENV["PORT"] ?? 3306;
+$pw = $_ENV["PW"];
+$user = $_ENV["USER"];
+$db = $_ENV["DB"];
+
+$dsn = "mysql:dbname=$db;host=$host;port=$port";
+
+echo $dsn . PHP_EOL;
+
+try {
+    $dbh = new PDO($dsn, $user, $pw); 
+}
+catch (Error $e) {
+    var_dump($e->getMessage());
+}
+
+$sql = "
+    CREATE TABLE IF NOT EXISTS counter (
+        cnt INT(11) NOT NULL DEFAULT 0
+    )
+";
+$dbh->query($sql);
+
+
+$sql = "SELECT COUNT(cnt) as cnt FROM counter";
+$q = $dbh->query($sql);
+$counter = $q->fetchColumn();
+echo "counter: " . $counter;
+
+
+$sql = "INSERT INTO counter (cnt) VALUES (1)";
+$dbh->query($sql);
+
+echo "<hr><div style='text-align:center;'>";
+echo "Powered by " . gethostname();
+echo "</div>";
+echo "alternative 1";
